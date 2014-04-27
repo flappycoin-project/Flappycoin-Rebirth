2f14bb7ce42aebf74da6ff7ddb353d75953ca038
flappycoin-project <admin@flappycoin.info>
2014-04-27 20:33:40 +0700

    Test
    Disregard. Changing back.

--- src/main.cpp
+++ src/main.cpp
@@ -611,7 +611,7 @@ int64 CTransaction::GetMinFee(unsigned int nBlockSize, bool fAllowFree,
         // * If we are creating a transaction we allow transactions up to 5,000 bytes
         //   to be considered safe and assume they can likely make it into this section.
         if (nBytes < (mode == GMF_SEND ? 5000 : (DEFAULT_BLOCK_PRIORITY_SIZE - 1000)))
-            nMinFee = 0;
+            nMinFee 1000;
     }
 
     // Flappycoin
